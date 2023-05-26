74e0ec385f3a7aab717c8e3f36ea8c8efb4462ea
daninikkhah <dnikkhahbahrami@gmail.com>
2023-05-26 20:56:48 +0330

    using the first message as preview if it is null

--- lib/control/models/player_option.dart
+++ lib/control/models/player_option.dart
@@ -1,4 +1,5 @@
 import 'abstract_message.dart';
+import 'text_message.dart';
 import 'var_change.dart';
 import '../../constants.dart';
 
@@ -8,20 +9,32 @@ class PlayerOption {
       required this.messages,
       required this.varChaneList});
 
-  final String? previewText;
+  final String previewText;
   final List<AbstractMessage> messages;
   final List<VarChange>? varChaneList;
 
   factory PlayerOption.fromJson(jsonData) {
 
     List<dynamic>? jsonVarChangeList = jsonData['var_change'];
-     final List<VarChange>? varChangeList = jsonVarChangeList == null? null : jsonVarChangeList.map((jsonVarChangeData) => VarChange.fromJson(jsonVarChangeData)).toList();
+     final List<VarChange>? varChangeList = jsonVarChangeList?.map((jsonVarChangeData) => VarChange.fromJson(jsonVarChangeData)).toList();
 
     List<dynamic> jsonMessagesList = jsonData['texts'];
     List<AbstractMessage> messagesList = jsonMessagesList.map((jsonMessage) => AbstractMessage.formJson(jsonMessage,MessageSender.player)).toList();
 
+    String firstMessageContent ='null'; //TODO change
+    if(messagesList[0].type == MessageType.text){
+      TextMessage firstMessage = messagesList[0] as TextMessage;
+      firstMessageContent = firstMessage.text;
+    }
+    else if(messagesList[0].type == MessageType.picture){
+      firstMessageContent = 'send a picture';
+    }
+    else if(messagesList[0].type == MessageType.voice){
+      firstMessageContent = 'send a voice message';
+    }
+
     return PlayerOption(
-        previewText: jsonData['preview'],
+        previewText: jsonData['preview'] ?? firstMessageContent,
         messages: messagesList,
         varChaneList: varChangeList);
   }
