e28600c123f5ff9644181c4c0efa52e4ad94833e
daninikkhah <dnikkhahbahrami@gmail.com>
2023-05-26 23:51:20 +0330

    gets full PlayerOption instead of text now

--- lib/widgets/bubble_chat_option.dart
+++ lib/widgets/bubble_chat_option.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import '../control/models/player_option.dart';
 
 const Color _primaryColor = Color(0xFFA129FF);
 
 class BubbleChatOption extends StatelessWidget {
-  const BubbleChatOption({Key? key, required this.text, required this.function})
+  const BubbleChatOption({Key? key,required this.option, required this.function})
       : super(key: key);
-  final String text;
-  final Function()? function;
+  final void Function(PlayerOption) function;
+  final PlayerOption option;
 
   @override
   Widget build(BuildContext context) {
@@ -14,29 +15,31 @@ class BubbleChatOption extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-          // MaterialStateProperty.all<EdgeInsetsGeometry>(
-          //     const EdgeInsets.symmetric(vertical: 15, horizontal: 10)),
-          backgroundColor: _primaryColor,
-          //MaterialStateProperty.all<Color>(_primaryColor),
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                  bottomLeft: Radius.circular(30))),
-          side: const BorderSide(color: Colors.white,width: 2)
-          // MaterialStateProperty.all<OutlinedBorder>(
-          //   const RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.only(
-          //           topLeft: Radius.circular(20),
-          //           topRight: Radius.circular(20),
-          //           bottomLeft: Radius.circular(20))),
-          // ),
-        ),
-        onPressed: function,
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            // MaterialStateProperty.all<EdgeInsetsGeometry>(
+            //     const EdgeInsets.symmetric(vertical: 15, horizontal: 10)),
+            backgroundColor: _primaryColor,
+            //MaterialStateProperty.all<Color>(_primaryColor),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30))),
+            side: const BorderSide(color: Colors.white, width: 2)
+            // MaterialStateProperty.all<OutlinedBorder>(
+            //   const RoundedRectangleBorder(
+            //       borderRadius: BorderRadius.only(
+            //           topLeft: Radius.circular(20),
+            //           topRight: Radius.circular(20),
+            //           bottomLeft: Radius.circular(20))),
+            // ),
+            ),
+        onPressed: () => function(option),
         child: Text(
-          text,
-          style: const TextStyle(fontSize: 18,),
+          option.previewText,
+          style: const TextStyle(
+            fontSize: 18,
+          ),
           softWrap: true,
         ),
       ),
