e1c75768e638e7dc2125d911ff90e2354a5d86d1
daninikkhah <dnikkhahbahrami@gmail.com>
2023-05-29 20:21:30 +0330

    variables list added

--- lib/control/models/scene_meta_data.dart
+++ lib/control/models/scene_meta_data.dart
@@ -1,26 +1,38 @@
+import 'variable.dart';
 
 class SceneMetaData {
   const SceneMetaData(
       {required this.currentElement,
       required this.tagToIdMap,
       required this.finalId,
-      required this.initialId});
+      required this.initialId,
+      required this.variables});
 
   final String currentElement;
   final Map<String, String> tagToIdMap;
   final String finalId;
   final String initialId;
+  final List<Variable> variables;
 
-  factory SceneMetaData.fromJson({String? currentElement, Map<String, dynamic>? tagToIdMap, String? finalId, String? initialId}) {
-    final Map<String,String> convertedTAgToIdMAp;
-    convertedTAgToIdMAp =  tagToIdMap == null? {} : tagToIdMap.map((key, value) => MapEntry(key, value.toString()));
+  factory SceneMetaData.fromJson(
+      {String? currentElement,
+      Map<String, dynamic>? tagToIdMap,
+      String? finalId,
+      String? initialId,
+      List<Variable>? variables}) {
+    final Map<String, String> convertedTAgToIdMAp;
+    convertedTAgToIdMAp = tagToIdMap == null
+        ? {}
+        : tagToIdMap.map((key, value) => MapEntry(key, value.toString()));
     return SceneMetaData(
-            currentElement: currentElement ?? ' null',
-            tagToIdMap: convertedTAgToIdMAp,
-            finalId: finalId ?? 'null',
-            initialId: initialId ?? 'null');
+        currentElement: currentElement ?? ' null',
+        tagToIdMap: convertedTAgToIdMAp,
+        finalId: finalId ?? 'null',
+        initialId: initialId ?? 'null',
+        variables: variables ?? []);
   }
 
   @override
-  String toString() => '{ currentElement: $currentElement, tagToIdMap: $tagToIdMap, finalId: $finalId, initialId: $initialId}';
+  String toString() =>
+      '{ currentElement: $currentElement, tagToIdMap: $tagToIdMap, finalId: $finalId, initialId: $initialId}';
 }
