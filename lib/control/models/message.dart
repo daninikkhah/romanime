e1451236324e490633f033ab66d59c999c699420
daninikkhah <dnikkhahbahrami@gmail.com>
2023-06-10 19:18:36 +0330

    message.dart fromLocalMap factory and toMap functions added

--- lib/control/models/message.dart
+++ lib/control/models/message.dart
@@ -1,15 +1,44 @@
 import '../../constants.dart';
-class Message{
+
+class Message {
   Message({required this.type, required this.sender, required this.value});
+
   final MessageType type;
   final MessageSender sender;
   final String value;
 
-  factory Message.formJson(dynamic jsonString, MessageSender sender){
+  factory Message.formJson(dynamic jsonString, MessageSender sender) =>
+      Message(value: jsonString, sender: sender, type: MessageType.text);
 
-    //TODO: add other message types
-    return Message(value: jsonString, sender: sender,type: MessageType.text);
+  factory Message.fromLocalMap(Map<String, String> localMapData){
+    // ignore: unrelated_type_equality_checks
+    MessageSender messageSender = localMapData['sender'] == MessageSender.player
+        ? MessageSender.player
+        : MessageSender.ai;
+    MessageType messageType;
+    switch (localMapData['type']) {
+      case 'MessageType.text':
+        messageType = MessageType.text;
+        break;
+      case 'MessageType.picture':
+        messageType = MessageType.picture;
+        break;
+      case 'MessageType.voice':
+        messageType = MessageType.voice;
+        break;
+      default:
+        messageType = MessageType.text;
+    }
+
+    return Message(type: messageType, sender: messageSender, value: localMapData['value']?? 'null');
   }
 
-  String toString()=> '{ value: $value , sender: ${sender == MessageSender.player? 'player' : 'ai'} }';
+  Map<String, String> toMap() =>
+      {'type': type.toString(), 'sender': sender.toString(), 'value': value};
+
+  @override
+  String toString() =>
+      '{ value: $value , sender: ${sender == MessageSender.player
+          ? 'player'
+          : 'ai'} }';
 }
