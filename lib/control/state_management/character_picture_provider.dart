d0a7ba5b400e0534c529f400a80f87e9ade1c720
Daniel Nikkhah Bahrami <dnikkhahbahrami@gmail.com>
2023-06-26 00:12:39 +0330

    now adds new image only if there is no existing image for the character

--- lib/control/state_management/character_picture_provider.dart
+++ lib/control/state_management/character_picture_provider.dart
@@ -1,21 +1,20 @@
-import'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
 import '../datasource/cloud_storage_datasource.dart';
 import '../models/character.dart';
 
-
-
-class CharactersPictureProvider with ChangeNotifier{
+class CharactersPictureProvider with ChangeNotifier {
   final Map<String, Future<Uint8List?>> _charactersImages = {};
-  Future<void> downloadImage(Character character) async{
-    Future<Uint8List?> image =  CloudStorageDatasource.getImage(character.pictureAddress);
-    _charactersImages.addAll({character.id : image});
-    // print('character id: '+ character.id+ 'image address: '+ character.pictureAddress + 'added' );
-    notifyListeners();
+
+  Future<void> downloadImage(Character character) async {
+    if(!_charactersImages.containsKey(character.id)) {
+      Future<Uint8List?> image =
+          CloudStorageDatasource.getImage(character.pictureAddress);
+      _charactersImages.putIfAbsent(character.id, () => image);
+      notifyListeners();
+    }
   }
 
   Future<Uint8List?>? getImage(String characterId) {
-   return _charactersImages[characterId];
+    return _charactersImages[characterId];
   } //TODO: check what happens if the key doesn't exist, is it even possible
-
-
 }
