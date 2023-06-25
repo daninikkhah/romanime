a722a106565b4a5d55410447666143c018420eb0
Daniel Nikkhah Bahrami <dnikkhahbahrami@gmail.com>
2023-06-26 00:10:59 +0330

    fixed the bug that caused chat screen not loading chatController after clicking on go to chat button

--- lib/control/state_management/matched_characters_provider.dart
+++ lib/control/state_management/matched_characters_provider.dart
@@ -4,43 +4,46 @@ import '../datasource/like_dislike_datasource.dart';
 import 'character_picture_provider.dart';
 import '../models/character.dart';
 import 'character_chat_controller_provider.dart';
+import 'character_picture_provider.dart';
 
-
-class MatchedCharactersProvider with ChangeNotifier{
-
+class MatchedCharactersProvider with ChangeNotifier {
   final List<Character> _matchedCharacters = [];
 
   List<Character> get matchedCharacters => _matchedCharacters;
 
-  Future<void> fetchMatchedCharacters(BuildContext context) async{
-    List<Character> matchedCharacters = await LikeDislikeDatasource.fetchLikedCharacters();
-    _matchedCharacters.addAll(matchedCharacters) ;
+  Future<void> fetchMatchedCharacters(BuildContext context) async {
+    List<Character> matchedCharacters =
+        await LikeDislikeDatasource.fetchLikedCharacters();
+    _matchedCharacters.addAll(matchedCharacters);
     for (var character in matchedCharacters) {
       await Future.wait([
-      Provider.of<CharacterChatControllerProvider>(context,listen: false).addNewCharacter(character.id),
-      Provider.of<CharactersPictureProvider>(context,listen: false).downloadImage(character)
-    ]);
-
+        Provider.of<CharacterChatControllerProvider>(context, listen: false)
+            .addNewCharacter(character.id),
+        Provider.of<CharactersPictureProvider>(context, listen: false)
+            .downloadImage(character),
+        Provider.of<CharactersPictureProvider>(context, listen: false)
+            .downloadImage(character),
+      ]);
     }
     notifyListeners();
   }
 
-
-
-  void like({required BuildContext context,required Character character, required Function matchedPopUpAlert}) async {
+  void like(
+      {required BuildContext context,
+      required Character character,
+      required Function matchedPopUpAlert}) async {
     if (character.isPlayable) {
       // todo: check if character is already liked
       _matchedCharacters.add(character);
-      Provider.of<CharacterChatControllerProvider>(context,listen: false).addNewCharacter(character.id);
+      Provider.of<CharacterChatControllerProvider>(context, listen: false)
+          .addNewCharacter(character.id);
       notifyListeners();
       matchedPopUpAlert();
     }
     LikeDislikeDatasource.like(character.id);
   }
 
-  void deleteCachedData(){
+  void deleteCachedData() {
     _matchedCharacters.removeWhere((element) => true);
   }
-
-
 }
